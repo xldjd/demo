c04ed97428a0d1cc0b71ab76d984c4d1fd5d43a7
xl <1485384932@qq.com>
2023-02-21 13:15:46 +0800

    first

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @Test
     void contextLoads() {
+redisTemplate.opsForValue().set("123","l123l");
+
     }
 
 }
